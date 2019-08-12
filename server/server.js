a3b0b98900b5f8b4a97bc0a3ee893b3efa2defca
Myron <dubtwee@gmail.com>
2019-08-12 00:17:08 -0500

    stubbing in a post endpoint for the Front-End to test with

--- server/server.js
+++ server/server.js
@@ -7,6 +7,11 @@ const PORT = 5000;
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post('/api/calculation', (req, res) => {
+    console.log(req.body);
+    res.send('OK');
+})
+
 app.listen(PORT, () => {
     console.log('Listening on port: ', PORT);
 });
