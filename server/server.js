f13b08ebdfd8347af530f25b3c593baaee9dda70
Myron <dubtwee@gmail.com>
2019-08-12 00:09:31 -0500

    adding the static directory to be served up from our node server app

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
     console.log('Listening on port: ', PORT);
 });
