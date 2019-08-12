4583a47528c9ab9301754993e6eca92b9f18b462
Myron <dubtwee@gmail.com>
2019-08-12 00:49:11 -0500

    adding the ability to store numbers and math operator all in an object to be posted to the server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,47 @@
 console.log('Hey there Isurus!!!');
+
+const calculationObject = {
+    numberOne: 0,
+    mathType: 'add',
+    numberTwo: 0,
+};
+
+$(document).ready(init);
+
+function init() {
+    console.log('Ready to go.');
+    $('.js-math').on('click', clickMathOperator);
+    $('.js-calculate').on('click', clickCalculate);
+    $('.js-clear').on('click', clickClear);
+}
+
+function clickMathOperator(event) {
+    const operatorBtn = $(this);
+    console.log(operatorBtn.data().mathType);
+    calculationObject.mathType = operatorBtn.data().mathType;
+}
+
+function clickCalculate(event) {
+    // get numbers and post to server
+    // store number one value
+    calculationObject.numberOne = $('.js-number-one').val();
+    calculationObject.numberTwo = $('.js-number-two').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/calculation',
+        data: calculationObject,
+    })
+    .then(function(response) {
+        console.log(response);
+    })
+}
+
+function clickClear(event) {
+    // clear all entered values
+    $('.js-number-one').val('');
+    $('.js-number-two').val('');
+
+    calculationObject.numberOne = 0;
+    calculationObject.numberTwo = 0;
+}
